61d8236e320b65a51b229c22df9c175597d76a96
Delchevalerie Valentin <47119234+ValDelch@users.noreply.github.com>
2024-07-04 10:49:16 +0200

    Update BesselConv2d.py

--- BesselConv/BesselConv2d.py
+++ BesselConv/BesselConv2d.py
@@ -106,6 +106,9 @@ class BesselConv2d(nn.Module):
             self.m_max = MASK.shape[0] - 1
             self.j_max = MASK.shape[1] - 1
 
+            if scale == 0:
+                _MASK = MASK
+
             # Add the transformation matrix to the list
             transMat = np.reshape(
                 np.transpose(transMat, (2,0,1,3)),
@@ -113,9 +116,10 @@ class BesselConv2d(nn.Module):
             )
             self.all_T.append(torch.from_numpy(transMat).to(self.device))
 
+        MASK = _MASK
+
         # Define the weights
-        #fan_in = self.C_in * (self.k**2)
-        fan_in = self.C_in
+        fan_in = self.C_in * (self.k**2)
 
         w_r_ini = np.random.normal(size=(self.m_max+1, self.j_max+1, self.C_in * self.C_out), loc=0., scale=np.sqrt(2./fan_in))
         # Remove parameters when k_mj > k_max
@@ -159,7 +163,7 @@ class BesselConv2d(nn.Module):
                 requires_grad=True
             )
             bound = 1. / np.sqrt(fan_in)
-            nn.init.uniform_(self.b, -bound, bound)
+            nn.init.normal_(self.b, mean=0., std=bound)
 
         # Get the number of parameters
         # For m = 0, no imaginary part
