38897f597adb0904805a9d1afc69c749420959a1
Delchevalerie Valentin <47119234+ValDelch@users.noreply.github.com>
2024-07-04 10:20:08 +0200

    Update BesselConv2d.py

--- BesselConv/BesselConv2d.py
+++ BesselConv/BesselConv2d.py
@@ -114,7 +114,8 @@ class BesselConv2d(nn.Module):
             self.all_T.append(torch.from_numpy(transMat).to(self.device))
 
         # Define the weights
-        fan_in = self.C_in * (self.k**2)
+        #fan_in = self.C_in * (self.k**2)
+        fan_in = (self.m_max+1) * (self.j_max+1) * self.C_in
 
         w_r_ini = np.random.normal(size=(self.m_max+1, self.j_max+1, self.C_in * self.C_out), loc=0., scale=np.sqrt(2./fan_in))
         # Remove parameters when k_mj > k_max
