010d9510a56bb49668e5e7745a049878ea30f948
Delchevalerie Valentin <47119234+ValDelch@users.noreply.github.com>
2024-08-27 13:48:35 +0200

    Update BesselConv2d.py

--- BesselConv/BesselConv2d.py
+++ BesselConv/BesselConv2d.py
@@ -131,7 +131,7 @@ class BesselConv2d(nn.Module):
         print('=====')
         for m in range(self.m_max+1):
             for j in range(self.j_max+1):
-                fan_in = self.C_in * (self.m_max+1) * (self.j_max+1) * np.sum(np.dot(transMat[m,:,j], np.conj(transMat[m,:,j])))
+                fan_in = self.C_in * (self.m_max+1) * (self.j_max+1) * np.sum(np.dot(transMat[m,:,j], np.conj(transMat[m,:,j])))**2
                 if fan_in == 0:
                     continue
                 print(fan_in)
